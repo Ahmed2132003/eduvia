dfe385911fef5af2d995323c61910195808a7d77
Ahmed2132003 <ahmedibrahim01122232957@gmail.com>
2025-07-08 02:36:06 +0300

    e

--- Eduvia/settings.py
+++ Eduvia/settings.py
@@ -97,7 +97,8 @@ LOCALE_PATHS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates',
+            BASE_DIR / 'skills_market/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
