02fdad55318ae5c839603e0a7357093caccfb892
eduvia-ai <eduvia.ai@gmail.com>
2026-05-14 09:53:26 +0300

    update

---
+++ marketplace/migrations/0005_merge_0003_alter_enrollment_student_related_name_0004_enrollmentcode_schema_sync.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("marketplace", "0003_alter_enrollment_student_related_name"),
+        ("marketplace", "0004_enrollmentcode_schema_sync"),
+    ]
+
+    operations = []
